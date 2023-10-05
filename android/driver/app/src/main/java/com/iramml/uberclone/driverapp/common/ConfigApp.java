50a29f2117a27f8d89e608ac273ab67909607f80
mpettaw2 <melisapettaway16@gmail.com>
2023-10-04 22:45:23 -0400

    adding changes for driver

--- android/driver/app/src/main/java/com/iramml/uberclone/driverapp/common/ConfigApp.java
+++ android/driver/app/src/main/java/com/iramml/uberclone/driverapp/common/ConfigApp.java
@@ -4,7 +4,7 @@ public class ConfigApp {
     public static final double baseFare = 2.55;
     public static final double timeRate = 0.35;
     public static final double distanceRate = 1.75;
-    public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAF3zIV-w:APA91bHjuWHfpd5-8_ZTkYC4_atq8nmcfvYPxgt12cle0IbOW6YAFhaZ3nvLZQj2GK30FBGVMrpsl3-kcu-0OOOtbvJgtCIIMNoOvsJaoy-ulDlnDSsMQD4w5b9EeTMp1mTs6kHTyZcY";
-    public static final String GOOGLE_API_KEY = "AIzaSyDfrhyZAZpsoVmz4NaVVJLZC91MINPGr9s";
+    public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAW_6s0lQ:APA91bGaTq6O0Jns19CDZEL8mG4eiTA2fU4ppQGI9zVwVHXehaVArhIof-C-Q3J5anQShFxHBjshXvMw52S-zsCuYqK9qayAPUq7Drme-qu3MowSfb70OJJuDUtdXjAeaHd-GHnOip8r";
+    public static final String GOOGLE_API_KEY = "AIzaSyAlkH-8rUwnwRJELdJIq_tx5jXCYQ0MmG4";
 
 }
